905f108eac24733be89e34c7c08837a25cdbfcbb
pranaykyasala <kyasalapranay@gmail.com>
2026-04-22 16:22:04 +0530

    additional code added

--- app.java
+++ app.java
@@ -3,3 +3,13 @@ public class Main {
        System.out.println("Hello World");
    }
 }
+
+
+public class AddTwoIntegers {
+   public static void main(String[] args) {
+       int first = 10;
+       int second = 20;
+       int sum = first + second;
+       System.out.println("The sum is: " + sum);
+   }
+}
